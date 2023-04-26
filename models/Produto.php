dc9b9a02aeea13c2f824c89cdf24d11478dcd5d8
Romildo Jr <fis.romildojr@gmail.com>
2023-04-25 21:50:39 -0300

    Aula 25-04-23

--- models/Produto.php
+++ models/Produto.php
@@ -1,63 +1,23 @@
 <?php
+require "C:/Users/r_jrs/Desktop/pw2_2023/models/Marca.php";
+require "C:/Users/r_jrs/Desktop/pw2_2023/models/Categoria.php";
 
 class Produto {
     private $id;
     private $nome;
-    private $valor;
-    private $descricao;
-    private $imagem;
+    private $percentual_lucro;
+    private $categoria;
+    private $marca;
 
-    public function __construct($id, $nome, $valor, $descricao, $imagem){
+    public function __construct($id, $nome, $percentual_lucro, Categoria $categoria, Marca $marca){
         $this->id = $id;
         $this->nome = $nome;
-        $this->valor = $valor;
-        $this->descricao = $descricao;
-        $this->imagem = $imagem;
+        $this->percentual_lucro = $percentual_lucro;
+        $this->categoria = $categoria;
+        $this->marca = $marca;
     }
    
-    public function getNome()
-    {
-        return $this->nome;
-    }
-
-    public function setNome($nome)
-    {
-        $this->nome = $nome;
-        return $this;
-    }
-
-    public function getValor()
-    {
-        return $this->valor;
-    }
-
-    public function setValor($valor)
-    {
-        $this->valor = $valor;
-        return $this;
-    }
-
-    public function getDescricao()
-    {
-        return $this->descricao;
-    }
-
-    public function setDescricao($descricao)
-    {
-        $this->descricao = $descricao;
-        return $this;
-    }
-
-    public function getImagem()
-    {
-        return $this->imagem;
-    }
-
-    public function setImagem($imagem)
-    {
-        $this->imagem = $imagem;
-        return $this;
-    }
+    
 
     /**
      * Get the value of id
@@ -75,6 +35,86 @@ class Produto {
     public function setId($id)
     {
         $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of percentual_lucro
+     */ 
+    public function getPercentual_lucro()
+    {
+        return $this->percentual_lucro;
+    }
+
+    /**
+     * Set the value of percentual_lucro
+     *
+     * @return  self
+     */ 
+    public function setPercentual_lucro($percentual_lucro)
+    {
+        $this->percentual_lucro = $percentual_lucro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categoria
+     */ 
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * Set the value of categoria
+     *
+     * @return  self
+     */ 
+    public function setCategoria(Categoria $categoria)
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of marca
+     */ 
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set the value of marca
+     *
+     * @return  self
+     */ 
+    public function setMarca(Marca $marca)
+    {
+        $this->marca = $marca;
 
         return $this;
     }
